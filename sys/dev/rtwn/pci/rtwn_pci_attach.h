19c13f42939ff40ed7187037e1cc9362af94b761
Farhan Khan <khanzf@gmail.com>
2017-01-12 01:36:44 -0500

    Added comment with Linux source

--- sys/dev/rtwn/pci/rtwn_pci_attach.h
+++ sys/dev/rtwn/pci/rtwn_pci_attach.h
@@ -20,6 +20,7 @@ void	r92ce_attach(struct rtwn_pci_softc *);
 
 enum {
 	RTWN_CHIP_RTL8192CE,
+	RTWN_CHIP_RTL8188EE,
 	RTWN_CHIP_MAX_PCI
 };
 
@@ -32,6 +33,7 @@ struct rtwn_pci_ident {
 
 static const struct rtwn_pci_ident rtwn_pci_ident_table[] = {
 	{ 0x10ec, 0x8176, "Realtek RTL8188CE", RTWN_CHIP_RTL8192CE },
+	{ 0x10ec, 0x8179, "Realtek RTL8188EE", RTWN_CHIP_RTL8188EE },
 	{ 0, 0, NULL, RTWN_CHIP_MAX_PCI }
 };
 
