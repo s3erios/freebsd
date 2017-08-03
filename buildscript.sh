aafe783bfdf9472d503f8c4bb827611bc718b565
Farhan Khan <khanzf@gmail.com>
2017-08-03 17:10:48 -0400

    rtwn-rtl8188eefw firmware loads

--- buildscript.sh
+++ buildscript.sh
@@ -3,8 +3,10 @@
 sudo sh -c "echo > /var/log/messages"
 sudo kldunload if_rtwn_pci
 sudo kldunload if_rtwn
+sudo kldunload rtwn-rtl8188eefw
 sudo make buildkernel -j 4 KERNCONF=GENERIC NO_CLEAN=1 MODULES_OVERRIDE="rtwn rtwn_pci rtwn" && sudo make reinstallkernel -j 4 KERNCONF=GENERIC MODULES_OVERRIDE="rtwn rtwn_pci rtwn"
 sudo sysctl kern.msgbuf_clear=1
 sudo kldload if_rtwn
 sudo kldload if_rtwn_pci
+sudo kldload rtwn-rtl8188eefw
 sudo sh -c "dmesg > /tank/shared/dmesg.freebsd"
