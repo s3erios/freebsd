2494133b86ee22ceb6a94a7ced07f82f94ab5ef8
Farhan Khan <khanzf@gmail.com>
2017-08-09 15:34:35 -0400

    Updated buildscript

--- buildscript.sh
+++ buildscript.sh
@@ -2,10 +2,10 @@
 
 sudo sh -c "echo > /var/log/messages"
 sudo kldunload if_rtwn_pci
-sudo kldunload if_rtwn
+sudo kldunload rtwn
 sudo kldunload rtwn-rtl8188eefw
 sudo make buildkernel -j 4 KERNCONF=GENERIC NO_CLEAN=1 MODULES_OVERRIDE="rtwn rtwn_pci rtwn" && sudo make reinstallkernel -j 4 KERNCONF=GENERIC MODULES_OVERRIDE="rtwn rtwn_pci"
-sudo kldload rtwn && sudo kldload if_rtwn_pci
+sudo kldload rtwn-rtl8188eefw && sudo kldload rtwn && sudo kldload if_rtwn_pci
 #sudo kldload rtwn-rtl8188eefw
 #sudo sh -c "dmesg > /tank/shared/dmesg.freebsd"
 #sudo ifconfig wlan0 create wlandev rtwn0
