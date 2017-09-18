085b83d232f08938adffea7c8438529928ba71b6
Farhan Khan <khanzf@gmail.com>
2017-09-18 00:03:10 -0400

    Screwed up load script. Whatever, don't care.

--- load.sh
+++ load.sh
@@ -10,20 +10,5 @@ sudo make -C sys/modules/rtwn/ unload
 sudo make -C sys/modules/rtwnfw/rtwnrtl8188ee/ unload
 
 sudo make -C sys/modules/rtwnfw/rtwnrtl8188ee/ load
-if [ $? ]; then
-        echo rtwnrtl8188ee failed to compile
-        exit 1
-fi
-
 sudo make -C sys/modules/rtwn/ load
-if [ $? ]; then
-        echo rtwn failed to compile
-        exit 1
-fi
-
 sudo make -C sys/modules/rtwn_pci/ load
-if [ $? ]; then
-        echo rtwn_pci failed to compile
-        exit 1
-fi
-
