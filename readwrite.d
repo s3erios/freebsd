ff94a12712f6951032f704c945b81ef25c6ae806
Farhan Khan <khanzf@gmail.com>
2017-09-21 21:30:38 -0400

    Added custom readwrite.d DTrace script to monitor read/write calls

---
+++ readwrite.d
@@ -0,0 +1,22 @@
+#!/usr/sbin/dtrace -s
+
+fbt::rtwn_pci_read_*:entry
+{
+	addr = arg1;
+}
+
+fbt::rtwn_pci_write_*:entry
+{
+	addr = arg1;
+	val = arg3;
+}
+
+fbt::rtwn_pci_read_1:return
+{
+	printf("%s( %x ) = %x\n", probefunc, addr, arg1 );
+}
+
+fbt::rtwn_pci_write_*:return
+{
+	printf("%s( %x %x )\n", probefunc, addr, val);
+}
